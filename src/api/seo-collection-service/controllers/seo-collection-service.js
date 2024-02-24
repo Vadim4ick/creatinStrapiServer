d759c91cc1cbba716f29e541eac49d1453297c2b
Vadim Firulyov <firulvv@mail.ru>
2024-02-24 22:07:47 +0400

    fix services seo

--- src/api/seo-collection-service/controllers/seo-collection-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * seo-collection-service controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::seo-collection-service.seo-collection-service');
--- src/api/seo-collection-service/services/seo-collection-service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * seo-collection-service service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::seo-collection-service.seo-collection-service');
