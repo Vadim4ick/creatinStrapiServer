59a4c453e11f44df4e0367f100e341cb62c6a7dd
Vadim Firulyov <firulvv@mail.ru>
2025-08-27 13:28:25 +0400

    Добавление картинок на мобилки для банера

---
+++ src/api/home-baner/routes/home-baner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-baner router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-baner.home-baner');
---
+++ src/api/home-baner/services/home-baner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-baner service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::home-baner.home-baner');
