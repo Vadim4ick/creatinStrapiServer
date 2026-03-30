fcfde3a88a7d478433ee981e8a25463ac5babeee
Vadim <a-yva-2013@mail.ru>
2026-03-30 15:11:05 +0400

    ♻️ (partnery): удаление ненужных файлов схемы, контроллера, маршрутов и сервиса Убраны файлы, связанные с сущностью "partnery", так как они больше не нужны в проекте. Это упрощает кодовую базу и уменьшает количество ненужных зависимостей, что способствует лучшей поддерживаемости и читаемости кода.

--- src/api/partnery/controllers/partnery.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * partnery controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::partnery.partnery');
